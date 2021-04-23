a15c56dd023c94bea022b971fafac5480b66d1d6
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-23 23:19:03 +0600

    saving-orders-to-firebase

--- src/components/Orders/Checkout/Checkout.js
+++ src/components/Orders/Checkout/Checkout.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
+const mapStateTopProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
+}
 class Checkout extends Component {
     state = {
         values: {
@@ -24,13 +33,30 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
-        console.log(this.state.values);
+        const order = {
+            ingredients: this.props.ingredients,
+            customer: this.state.values,
+            price: this.props.totalPrice,
+            orderTime: new Date(),
+        }
+        // we must write .json after the key while using firebase
+        axios.post("https://happy-burger-d182b-default-rtdb.firebaseio.com/orders.json", order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
     }
 
     render() {
         return (
             <div>
-                <h4 style={{ backgroundColor: "#00AFDB", color: "white", padding: "15px", margin: "0px", borderTopLeftRadius: "5px", border: "1px solid grey", borderTopRightRadius: "5px" }}>Checkout</h4>
+                <h4 style={{
+                    backgroundColor: "#00AFDB",
+                    color: "white",
+                    border: "1px solid grey",
+                    margin: "0px",
+                    borderTopLeftRadius: "5px",
+                    borderTopRightRadius: "5px",
+                    padding: "20px",
+                }}>Payment: {this.props.totalPrice} BDT</h4>
                 <form style={{
                     border: "1px solid grey",
                     // boxShadow: "1px 1px #888888",
@@ -40,21 +66,41 @@ class Checkout extends Component {
                     borderTopLeftRadius: "0px",
                     borderTopRightRadius: "0px"
                 }}>
-                    <textarea name="deliveryAddress" value={this.state.values.deliveryAddress} className="form-control" placeholder="Your Address" onChange={(e) => this.inputChangerHandler(e)}></textarea>
+                    <textarea
+                        name="deliveryAddress"
+                        value={this.state.values.deliveryAddress}
+                        className="form-control" placeholder="Your Address"
+                        onChange={(e) => this.inputChangerHandler(e)}
+                    />
                     <br />
-                    <input name="phone" className="form-control" value={this.state.values.phone} placeholder="Your Phone Number" onChange={(e) => this.inputChangerHandler(e)} />
+                    <input
+                        name="phone"
+                        className="form-control"
+                        value={this.state.values.phone}
+                        placeholder="Your Phone Number"
+                        onChange={(e) => this.inputChangerHandler(e)}
+                    />
                     <br />
-                    <select name="paymentType" className="form-control" value={this.state.values.paymentType} onChange={(e) => this.inputChangerHandler(e)}>
+                    <select
+                        name="paymentType"
+                        className="form-control"
+                        value={this.state.values.paymentType}
+                        onChange={(e) => this.inputChangerHandler(e)
+                        }>
                         <option value="Cash On Delivery">Cash On Delivery</option>
                         <option value="Bkash">Bkash</option>
                     </select>
                     <br />
-                    <Button color="danger" className="ml-3 float-right" onClick={this.goBack}>Cancel</Button>
-                    <Button color="success" className="mr-auto float-right" onClick={this.submitHandler}>Place Order</Button>
+                    <Button color="danger" className="ml-3 float-right" onClick={this.goBack}>
+                        Cancel
+                    </Button>
+                    <Button color="success" className="mr-auto float-right" onClick={this.submitHandler}>
+                        Place Order
+                    </Button>
                 </form>
             </div >
         )
     }
 }
 
-export default Checkout;
+export default connect(mapStateTopProps)(Checkout);
