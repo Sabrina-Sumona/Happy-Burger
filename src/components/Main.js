3b8a75a3e8fdb0a3be87fe23468df1f8e582fb6a
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-24 22:33:55 +0600

    using-local-storage-to-store-token

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header/Header';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 import Orders from './Orders/Orders';
@@ -6,6 +6,7 @@ import Checkout from './Orders/Checkout/Checkout';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Auth from './Auth/Auth';
 import { connect } from 'react-redux';
+import { authCheck } from '../redux/authActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -13,35 +14,45 @@ const mapStateToProps = state => {
     }
 }
 
-const Main = props => {
+const mapDispatchToProps = dispatch => {
+    return {
+        authCheck: () => dispatch(authCheck()),
+    }
+}
 
-    let routes = null;
-    if (props.token === null) {
-        routes = (
-            <Switch>
-                <Route path="/login" component={Auth} />
-                <Redirect to="/login" />
-            </Switch>
-        )
-    } else {
-        routes = (
-            <Switch>
-                <Route path="/orders" component={Orders} />
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/" exact component={BurgerBuilder} />
-                <Redirect to="/" />
-            </Switch>
+class Main extends Component {
+    componentDidMount() {
+        this.props.authCheck();
+    }
+    render() {
+        let routes = null;
+        if (this.props.token === null) {
+            routes = (
+                <Switch>
+                    <Route path="/login" component={Auth} />
+                    <Redirect to="/login" />
+                </Switch>
+            )
+        } else {
+            routes = (
+                <Switch>
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Redirect to="/" />
+                </Switch>
+            )
+        }
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    {routes}
+                </div>
+            </div>
         )
     }
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                {routes}
-            </div>
-        </div>
-    )
 }
 
 
-export default connect(mapStateToProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- src/redux/authActionCreators.js
+++ src/redux/authActionCreators.js
@@ -28,10 +28,26 @@ export const auth = (email, password, mode) =>
         const API_KEY = "AIzaSyDkBFrFFNrRZajx5N52G2PO6CwpAyd5Xcw";
         axios.post(authUrl + API_KEY, authData)
             .then(response => {
-                // localStorage.setItem('token', response.data.idToken);
-                // localStorage.setItem('userId', response.data.localId);
-                // const expirationTime = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                // localStorage.setItem('expirationTime', expirationTime);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('userId', response.data.localId);
+                // sec to mili for that 1000 is used
+                const expirationTime = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('expirationTime', expirationTime);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
             })
     }
+
+export const authCheck = () => dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        // Logout
+    } else {
+        const expirationTime = new Date(localStorage.getItem('expirationTime'));
+        if (expirationTime <= new Date()) {
+            // Logout
+        } else {
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId));
+        }
+    }
+}
